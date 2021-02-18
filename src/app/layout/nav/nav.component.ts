172f3ff92b9203a43ede9786300f0ec229c34b68
Rano Kuhl <ranokuhl@gmail.com>
2021-02-18 03:12:21 +0100

    demo completed

--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Person {
+  name: string;
+  blogurl: string;
+  twitter: string;
+}
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -7,7 +13,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+
   constructor() { }
+
+  people: Person[] = [
+    {name: 'Cody De Arkland', blogurl: 'https://www.thehumblelab.com', twitter: '@CodyDeArkland'},
+    {name: 'Grant Orchard', blogurl: 'https://www.grantorchard.com', twitter: '@GrantOrchard'},
+    {name: 'Jon Schulman', blogurl: 'http://www.vaficionado.com', twitter: '@vAficionado'},
+    {name: 'Jad El-Zein', blogurl: 'https://www.virtualjad.com', twitter: '@virtualjad'},
+    {name: 'Tony Reeves', blogurl: 'https://www.digitalvspace.com', twitter: '@importcarguy'},
+    {name: 'Al Rasheed', blogurl: 'https://alarasheedblog.wordpress.com/', twitter: '@al_rasheed'}
+  ];
 
   ngOnInit(): void {
   }
